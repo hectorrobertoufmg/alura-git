b57d0f622c300f1cdb502243937ff4ca54be1e00
Hector Roberto <hectorroberto7543@gmail.com>
2023-05-07 17:22:36 -0300

    Nova variavel foi criada

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 
 int main()
 {
-
+    
     printf("Teste");
     printf("Linha adicionada dentro do Repositório do Hector!");
     printf("Linha adicionada dentro do Repositório da Raquel!");
+    
+    int idade = 30;
+    
+    
     return 0;
 }
