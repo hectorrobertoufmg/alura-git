06d47e3074a65263b122dd3e7d7b594ee2ff3255
Raquel Nunes <raquelnunes@gmail.com>
2023-05-07 17:01:07 -0300

    Linha adicionada pelo repositório da Raquel Nunes

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main()
 
     printf("Teste");
     printf("Linha adicionada dentro do Repositório do Hector!");
+    printf("Linha adicionada dentro do Repositório da Raquel!");
     return 0;
 }
